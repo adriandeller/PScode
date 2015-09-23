0a0d76eac37e905fdd6b733da80f16403c98a06f
adriandeller <adrian.deller@unibas.ch>
2015-09-23 15:35:21 +0200

    Update Check-IsAdmin.ps1

--- Check-IsAdmin.ps1
+++ Check-IsAdmin.ps1
@@ -1,5 +1,5 @@
 function Get-IsAdministrator
-    {
+{
     $WindowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $WindowsPrincipal = [Security.Principal.WindowsPrincipal] $WindowsIdentity
     if ($WindowsPrincipal.IsInRole([Security.Principal.WindowsBuiltinRole] "Administrator")) {
