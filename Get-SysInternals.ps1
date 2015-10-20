2547f835efc7b104519190f9d3005119314a0792
adriandeller <adrian.deller@unibas.ch>
2015-10-20 17:23:23 +0200

    Update Get-SysInternals.ps1

--- Get-SysInternals.ps1
+++ Get-SysInternals.ps1
@@ -68,7 +68,7 @@ Begin
 
             $Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
             $OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
-            $NewPath = $OldPath + ';' + $Path
+            $NewPath = $OldPath + ";" + $Path
             Set-ItemProperty -Path "$Reg" -Name PATH –Value $NewPath -Confirm:$false
         }
     }
