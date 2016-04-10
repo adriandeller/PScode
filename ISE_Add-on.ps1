ad8a4a368c126d7babcf6ea8d375443a077a47ba
Adrian Deller <adrian@deller.ch>
2016-04-10 20:29:10 +0200

    added files with functions

---
+++ ISE_Add-on.ps1
@@ -0,0 +1,38 @@
+﻿<#
+ # from https://learn-powershell.net/2016/02/01/quickly-create-a-powershell-snippet-in-the-ise-using-an-add-on/
+#>
+
+[void]$psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add("Save selection as Snippet",{
+    $Text = $psISE.CurrentFile.Editor.SelectedText
+    If (($Text -notmatch '^\s+$' -AND $Text.length -gt 0)) {
+        Try {
+            [void][Microsoft.VisualBasic.Interaction]
+        } Catch {
+            Add-Type –assemblyName Microsoft.VisualBasic
+        }
+        $Name = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Snippet Name", "Snippet Name")
+        $Description = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Snippet Description", "Snippet Description")
+        If ($Name -and $Description) {
+            New-IseSnippet -Description $Description -Title $Name -Text $Text –Force
+            Write-Host "New Snippet created!" -ForegroundColor Yellow -BackgroundColor Black
+        }
+    }
+},"Alt+F8")
+
+
+[void]$psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add("Save whole script as Snippet",{
+    $Text = $psISE.CurrentFile.Editor.Text
+    If (($Text -notmatch '^\s+$' -AND $Text.length -gt 0)) {
+        Try {
+            [void][Microsoft.VisualBasic.Interaction]
+        } Catch {
+            Add-Type –assemblyName Microsoft.VisualBasic
+        }
+        $Name = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Snippet Name", "Snippet Name")
+        $Description = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Snippet Description", "Snippet Description")
+        If ($Name -and $Description -AND ($Text -notmatch '^\s+$' -AND $Text.length -gt 0)) {
+            New-IseSnippet -Description $Description -Title $Name -Text $Text –Force
+            Write-Host "New Snippet created!" -ForegroundColor Yellow -BackgroundColor Black
+        }
+    }
+},"Alt+F5")
---
+++ SetupDevClient.ps1
@@ -0,0 +1,5 @@
+﻿# https://github.com/dahlbyk/posh-git
+Install-Module Posh-Git | Install-Module -Scope CurrentUser
+
+# https://github.com/drmohundro/Find-String
+Install-Module Find-String | Install-Module -Scope CurrentUser
